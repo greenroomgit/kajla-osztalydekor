6f8dcdf41121ce798bebaa5fa5e945cb82aeb396
Gergely Olah <olah.gergely@pixeloid.hu>
2021-04-28 11:09:44 +0200

    Initialize dependabot/npm_and_yarn/lodash-4.17.19

---
+++ resources/js/components/pages/makettek-letoltese.vue
@@ -0,0 +1,23 @@
+<template>
+    <Kits />
+</template>
+
+<script>
+    import Kits from '../kits';
+
+    export default {
+        name: 'kits',
+        components: {
+            'Kits': Kits,
+        },
+    metaInfo: {
+        // if no subcomponents specify a metaInfo.title, this title will be used
+        // all titles will be injected into this template
+        title: 'Letölthető makettek'
+    }
+
+    }
+
+</script>
+
+
---
+++ resources/js/store.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+export default Vue.observable({
+  photoUploadModalIsVisible: false,
+  kitRequestModalIsVisible: false,
+  CookieModalIsVisible: false,
+});
