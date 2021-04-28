6c883d53489dcf963056d15e18994f4c8a7c860f
Gergely Olah <olah.gergely@pixeloid.hu>
2021-04-28 11:09:41 +0200

    Initial commit

---
+++ resources/js/components/jackpot.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="columns is-centered">
+        <div :class="[
+                    'column ', 
+                    !isWide ? 'is-four-fifths' : 'wide'
+                    ]">
+            <div :class="[
+                    'jackpot-box separator', 
+                    isWide ?  'wide' : ''
+                    ]">
+                <div class="jackpot-box__inner">
+                    <div class="columns is-vcentered    ">
+                        <div class="column">
+                            <h2 class="title is-size-3 is-size-4-tablet is-size-3-desktop has-text-centered has-text-left-tablet">
+                                A fődíj Magyarország térképe, melyen elhelyezheted a 20 darabos makett csomag minden nevezetességét.
+                            </h2>
+                        </div>
+                        <div class="column jackpot-map-item">
+                            <div class="jackpot-map">
+                                <img src="/assets/images/terkep_makett_fodij.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isWide: Boolean,
+    }
+}
+</script>
---
+++ resources/views/emails/success-request.blade.php
@@ -0,0 +1,12 @@
+@extends('emails.kajla')
+
+@section('content')
+
+	@include('emails.kajla.contentStart')
+<p><b>Kedves Játékosunk!</b></p>
+
+<br>
+
+	@include('emails.kajla.contentEnd')
+
+@stop
